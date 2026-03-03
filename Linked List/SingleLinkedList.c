a991c1caecad2d25484adf9e5440c807c1ef8a37
Nath2006 <ananthapindika739@gmail.com>
2026-03-03 08:17:47 +0700

    delete Single Linked List

--- Linked List/SingleLinkedList.c
+++ Linked List/SingleLinkedList.c
@@ -23,16 +23,16 @@ void masukinDataDariDepan(struct tnode **head, int x){
   //alokasikan memori untuk node baru
   struct tnode *node = (struct tnode*) malloc(sizeof(struct tnode));
 
-  //isi data
-  node->value = x;
-  node->next = *head;
+  
+  node->value = x; //isi data
+  node->next = *head; //node baru tunjuk ke head lama
 
   //head sekarang node baru
   *head = node;
 }
 
 /*Masukin data dari belakang(dari tail)*/
-void masukinDataDariBelaknag(struct tnode **head, int x){
+void masukinDataDariBelakang(struct tnode **head, int x){
   //alokasikan memori untuk node baru
   struct tnode *node = (struct tnode*) malloc(sizeof(struct tnode));
 
@@ -71,6 +71,31 @@ void masukinDataDariTengah(struct tnode *prev_node, int x){
 }
 
 
+void deleteNode(struct tnode **head, int x){
+  if (head == NULL) return; //jika list kosong, tidak ada data yang di hapus
+
+  struct tnode *curr = *head;
+
+  //kondisi 1: x ada di HEAD
+  if (curr->value == x){
+    *head = curr->next; //head maju ke node berikutnya 
+    free(curr); //bebaskan node pertama
+    return; //selesai
+  }
+
+  //kondisi 2: x bukan di head, kita harus traversal sampai ketemu
+  //kita berhenti saat curr->next adalah node target
+  while(curr->next != NULL && curr->next->value != x){
+    curr = curr->next;
+    
+    if(curr->next == NULL) return;  //jika data tidak ada dalam list
+
+    struct tnode *del = curr->next; //node yang akan dihapus 
+    curr->next = del->next; //skip del: hubungkan curr ke node berikutnya dari setelah del
+    free(del);
+  }
+}
+
 void printList(struct tnode *head) {
     struct tnode *curr = head;
     while (curr != NULL) {
@@ -99,16 +124,20 @@ int main(){
   printList(head);
   masukinDataDariDepan(&head, 10);
   printList(head);
-  masukinDataDariDepan(&head, 20);
-    printList(head);
-  masukinDataDariDepan(&head, 30);
-    printList(head);
-  masukinDataDariDepan(&head, 40);
-    printList(head);
-  masukinDataDariBelaknag(&head, 1000);
+  masukinDataDariBelakang(&head, 20);
   printList(head);
-  masukinDataDariTengah(head, 69);
+  masukinDataDariTengah(head, 25);
   printList(head);
+  deleteNode(&head, 20);
+  printList(head);
+  // masukinDataDariDepan(&head, 30);
+  //   printList(head);
+  // masukinDataDariDepan(&head, 40);
+  //   printList(head);
+  // masukinDataDariBelaknag(&head, 1000);
+  // printList(head);
+  // masukinDataDariTengah(head, 69);
+  // printList(head);
 
   return 0;
 }
